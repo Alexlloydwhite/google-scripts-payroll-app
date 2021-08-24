38fbb76112515e33e1d232b74048f330ef1dd841
Alex White <alexlloydwhite@gmail.com>
2021-08-24 11:15:13 -0500

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,6 @@ const employeeGratuityPayoutPercentage = 0.475;
 function CalculatePayroll() {
   let ss = SpreadsheetApp.getActiveSpreadsheet();
   let formS = ss.getSheetByName("Form");
-  let accountingS = ss.getSheetByName("Accounting History");
   let payrollPeriod = formS.getRange("C5").getValue();
   let grossTotal = formS.getRange("C3").getValue();
   let taxesPaid = (grossTotal * mnTaxRate).toFixed(2);
@@ -15,59 +14,6 @@ function CalculatePayroll() {
   let employeeGratuityPayout = (
     netTotal * employeeGratuityPayoutPercentage
   ).toFixed(2);
-
-  // Run function to calculate employee payout
-  EmployeePayout(payrollPeriod, employeeGratuityPayout);
-
-  // Clear inputs
-  ClearCell();
-}
-
-// Function clears all cells in the form
-function ClearCell() {
-  let ss = SpreadsheetApp.getActiveSpreadsheet();
-  let formS = ss.getSheetByName("Form");
-
-  let rangesToClear = [
-    "C3",
-    "C5", // Inputs
-    "C8",
-    "C9",
-    "C10",
-    "C11",
-    "C12",
-    "C13",
-    "C14",
-    "C15",
-    "C16",
-    "C17",
-    "C18", // Employee Hours Worked
-  ];
-
-  // Loop over Range and clear contents
-  for (let i = 0; i < rangesToClear.length; i++) {
-    formS.getRange(rangesToClear[i]).clearContent();
-  }
-}
-
-function insertRow(sheet, rowData) {
-  let lock = LockService.getScriptLock();
-  lock.waitLock(30000);
-  try {
-    sheet
-      .insertRowBefore(2)
-      .getRange(2, 1, 1, rowData.length)
-      .setValues([rowData]);
-    SpreadsheetApp.flush();
-  } finally {
-    lock.releaseLock();
-  }
-}
-
-function EmployeePayout(payrollPeriod, employeeGratuityPayout) {
-  let ss = SpreadsheetApp.getActiveSpreadsheet();
-  let formS = ss.getSheetByName("Form");
-  let payoutS = ss.getSheetByName("Employee Payout History");
   let totalHoursWorked = 0;
   let employeeNameRange = [
     "B8",
@@ -105,21 +51,128 @@ function EmployeePayout(payrollPeriod, employeeGratuityPayout) {
   // Calculate hourly gratuity based on total hours worked and payout %
   let hourlyGratuity = (employeeGratuityPayout / totalHoursWorked).toFixed(2);
 
+  // Run function to calculate employee payout
+  EmployeePayout(
+    payrollPeriod,
+    hourlyGratuity,
+    employeeNameRange,
+    employeeHoursRange
+  );
+
+  // Run function to calculate accounting history
+  AccountingHistory(
+    payrollPeriod,
+    grossTotal,
+    taxesPaid,
+    netTotal,
+    employeeGratuityPayout,
+    totalHoursWorked,
+    hourlyGratuity
+  );
+
+  // Clear inputs
+  ClearCell();
+}
+
+// Function clears all cells in the form
+function ClearCell() {
+  let ss = SpreadsheetApp.getActiveSpreadsheet();
+  let formS = ss.getSheetByName("Form");
+
+  let rangesToClear = [
+    "C3",
+    "C5",
+    "C8",
+    "C9",
+    "C10",
+    "C11",
+    "C12",
+    "C13",
+    "C14",
+    "C15",
+    "C16",
+    "C17",
+    "C18",
+  ];
+
+  // Loop over Range and clear contents
+  for (let i = 0; i < rangesToClear.length; i++) {
+    formS.getRange(rangesToClear[i]).clearContent();
+  }
+}
+
+function insertRow(sheet, rowData) {
+  let lock = LockService.getScriptLock();
+  lock.waitLock(30000);
+  try {
+    sheet
+      .insertRowBefore(2)
+      .getRange(2, 1, 1, rowData.length)
+      .setValues([rowData]);
+    SpreadsheetApp.flush();
+  } finally {
+    lock.releaseLock();
+  }
+}
+
+function EmployeePayout(
+  payrollPeriod,
+  hourlyGratuity,
+  employeeNameRange,
+  employeeHoursRange
+) {
+  let ss = SpreadsheetApp.getActiveSpreadsheet();
+  let formS = ss.getSheetByName("Form");
+  let payoutS = ss.getSheetByName("Employee Payout History");
+
   // Loop over employee range, append each employee record to Payout History table
   for (let i = 0; i < employeeNameRange.length; i++) {
     let employeeName = formS.getRange(employeeNameRange[i]).getValue();
     let employeeHours = formS.getRange(employeeHoursRange[i]).getValue();
     let hourlyGratuityPayout = hourlyGratuity * employeeHours;
 
+    let rowData = [
+      payrollPeriod,
+      employeeName,
+      employeeHours,
+      hourlyGratuityPayout,
+    ];
+
     // Validate inputs, append rows
-    if (employeeName !== "" && employeeHours !== "") {
-      let rowData = [
-        payrollPeriod,
-        employeeName,
-        employeeHours,
-        hourlyGratuityPayout,
-      ];
+    if (employeeName && employeeHours) {
       insertRow(payoutS, rowData);
     }
+  }
+}
+
+function AccountingHistory(
+  payrollPeriod,
+  grossTotal,
+  taxesPaid,
+  netTotal,
+  employeeGratuityPayout,
+  totalHoursWorked,
+  hourlyGratuity
+) {
+  let ss = SpreadsheetApp.getActiveSpreadsheet();
+  let accountingHistorySheet = ss.getSheetByName("Accounting History");
+
+  let quartlyBonusFund = (netTotal * quartlyBonusFundPercentage).toFixed(2);
+  let foodCost = (netTotal * foodCostPercentage).toFixed(2);
+
+  let rowData = [
+    payrollPeriod,
+    grossTotal,
+    taxesPaid,
+    netTotal,
+    quartlyBonusFund,
+    foodCost,
+    employeeGratuityPayout,
+    totalHoursWorked,
+    hourlyGratuity,
+  ];
+
+  if (payrollPeriod && grossTotal) {
+    insertRow(accountingHistorySheet, rowData);
   }
 }
